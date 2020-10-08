ace17a015b2d23deb89db12aa4de5a7751f4656a
azizul016 <haqueazizul789@gmail.com>
2020-10-08 21:08:06 +0600

    success

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ client.connect(err => {
 
     //get method;
     app.get("/products", (req, res) => {
-        productsCollection.find({})
+        const search = req.query.search
+        productsCollection.find({ name: { $regex: search } })
             .toArray((err, documents) => {
                 res.send(documents)
             })
